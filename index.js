c2d4f10bc660c658389feffd536aad689624a7e0
Vartit Garg <vartit.garg@rupeek.com>
2023-08-04 13:38:44 +0530

    additional changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const {qs} = require('qs');
+const fs = require('fs');
 
 const runScript = async() => {
     try {
@@ -8,16 +9,16 @@ const runScript = async() => {
           url: `http://127.0.0.1:3333/command/core/get-csrf-token`,
           method: 'GET',
         };
-        console.log(fetchtokenoptions,'fetchtokenoptions');
+        // console.log(fetchtokenoptions,'fetchtokenoptions');
         const fetchtokenresponse = await axios(fetchtokenoptions);
-        console.log(fetchtokenresponse.data,'response');
+        // console.log(fetchtokenresponse.data,'response');
         const createProjectOptions = {
             url: `http://127.0.0.1:3333/command/core/create-project-from-upload?csrf_token=${fetchtokenresponse.data.token}`,
             method: 'POST',
           };
-        console.log(createProjectOptions,'createProjectOptions');
+        // console.log(createProjectOptions,'createProjectOptions');
         const createProjectResponse = await axios(createProjectOptions);
-        console.log(createProjectResponse,'createProjectResponse');    
+        // console.log(createProjectResponse,'createProjectResponse');    
         const fetchtokenresponse2 = await axios(fetchtokenoptions);
         const applyOperationsOptions = {
             url: `http://127.0.0.1:3333/command/core/apply-operations?project=1811607108481&csrf_token=${fetchtokenresponse2.data.token}&operations=[
@@ -37,23 +38,52 @@ const runScript = async() => {
               ]`,
             method: 'POST',
           };
-        console.log(applyOperationsOptions,'applyOperationsOptions');
+        // console.log(applyOperationsOptions,'applyOperationsOptions');
         const applyOperationsResponse = await axios(applyOperationsOptions);
-        console.log(applyOperationsResponse.data,'applyOperationsResponse');    
+        // console.log(applyOperationsResponse.data,'applyOperationsResponse');    
         
         
         const exportDataOptions = {
             url: `http://127.0.0.1:3333/command/core/export-rows/abcd.csv`,
             method: 'POST',
-            data: qs.stringify({
+            headers:   JSON.parse(JSON.stringify({ 
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 
+                'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8', 
+                'Cache-Control': 'max-age=0', 
+                'Connection': 'keep-alive', 
+                'Content-Type': 'application/x-www-form-urlencoded', 
+                'Cookie': 'host=.butterfly; host=.butterfly', 
+                'Origin': 'http://127.0.0.1:3333', 
+                'Referer': 'http://127.0.0.1:3333/project?project=2138472716692', 
+                'Sec-Fetch-Dest': 'document', 
+                'Sec-Fetch-Mode': 'navigate', 
+                'Sec-Fetch-Site': 'same-origin', 
+                'Sec-Fetch-User': '?1', 
+                'Upgrade-Insecure-Requests': '1', 
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36', 
+                'sec-ch-ua': '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"', 
+                'sec-ch-ua-mobile': '?0', 
+                'sec-ch-ua-platform': '"macOS"'
+            })),
+            maxBodyLength: Infinity,
+            data: JSON.parse(JSON.stringify({
                 'project': '1811607108481',
                 'format': 'csv',
+                'quoteAll': '',
                 'engine': '{"facets":[],"mode":"row-based"}' 
-            })
+              }))
         };
-          console.log(exportDataOptions,'exportDataOptions');
+        //   console.log(exportDataOptions,'exportDataOptions');
           const exportDataResponse = await axios(exportDataOptions);
-          console.log(exportDataResponse);
+          console.log(exportDataResponse.data);
+          fs.writeFile("./output.csv",exportDataResponse.data, err=> {
+            if(!err){
+                console.log('File written successfully');
+            }
+            else{
+                console.log('error occured')
+            }
+          })
       } catch (error) {
         console.log('error');
       }
